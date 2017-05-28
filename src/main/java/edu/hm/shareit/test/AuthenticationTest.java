029b2d5fd91f93e0977e49dfcaf969fc6390c380
whaph <whaph@outlook.com>
2017-05-28 15:44:13 +0200

    + tests

--- src/main/java/edu/hm/shareit/test/AuthenticationTest.java
+++ src/main/java/edu/hm/shareit/test/AuthenticationTest.java
@@ -3,6 +3,7 @@ package edu.hm.shareit.test;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,38 +22,89 @@ import static edu.hm.shareit.auth.Authentication.*;
  * Created by aykut on 28.05.2017.
  */
 public class AuthenticationTest {
-    final String logInData = "{\"username\" : \"Rick Astley\", \"password\" : \"Never gonna give you up!\"}";
     static final Client CLIENT = ClientBuilder.newClient();
     static final WebTarget LOG_IN_TARGET = CLIENT.target("http://localhost:8080").path("shareit/auth/login");
     static final WebTarget AUTHORIZE_TARGET = CLIENT.target("http://localhost:8080").path("shareit/auth/authorize");
     static final LoginAttempt entity = new LoginAttempt("Rick Astley", "Never gonna give you up!");
-    Server jetty;
+
     static final String WEBAPP_DIR = "./src/main/webapp/";
     static final String APP_URL = "/";
+
+    static Server jetty;
+
+    final String logInData = "{\"username\" : \"Rick Astley\", \"password\" : \"Never gonna give you up!\"}";
+    final String logInFailure = "{\"username\" : \"Rick\", \"password\" : \"Never gonna give!\"}";
 
     @Before
     public void setUp() throws Exception {
         jetty = new Server(8080);
         jetty.setHandler(new WebAppContext(WEBAPP_DIR, APP_URL));
         jetty.start();
+        //jetty.join();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        jetty.stop();
+        jetty.join();
+        //jetty.destroy();
     }
 
     @Test
     public void firstLogIn() throws Exception {
         final Response have = LOG_IN_TARGET
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .post(Entity.entity(convertToJson(entity), MediaType.APPLICATION_JSON_TYPE));
-        System.out.println(convertToJson(entity));
-        System.out.println("---------------------AMK");
-        System.out.println(have.getEntity());
-        System.out.println("---------------------AMK");
+                .post(Entity.entity(logInData, MediaType.APPLICATION_JSON_TYPE));
         final ObjectMapper mapper = new ObjectMapper();
 
-        Token token = mapper.readValue(have.getEntity().toString(), Token.class);
+        final Token token = mapper.readValue(have.readEntity(String.class), Token.class);
 
         assertTrue(have.getStatus() == 200 && token.getToken().length() == 50);
     }
 
+    @Test
+    public void multipleLogIns() throws Exception {
+        for(int i = 0; i < 10; i++) {
+            final Response have = LOG_IN_TARGET
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .post(Entity.entity(logInData, MediaType.APPLICATION_JSON_TYPE));
+            final ObjectMapper mapper = new ObjectMapper();
+
+            final Token token = mapper.readValue(have.readEntity(String.class), Token.class);
+
+            assertTrue(have.getStatus() == 200 && token.getToken().length() == 50);
+        }
+    }
+
+    @Test
+    public void firstLogInFail() throws Exception {
+        final Response have = LOG_IN_TARGET
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .post(
+                        Entity.entity(
+                                logInFailure
+                                , MediaType.APPLICATION_JSON_TYPE
+                        )
+                );
+        System.out.println(have.getStatus());
+        assertTrue(have.getStatus() == 404);
+    }
+
+    @Test
+    public void firstLogInFailTenTimes() throws Exception {
+        for(int i = 0; i < 10; i++) {
+            final Response have = LOG_IN_TARGET
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .post(
+                            Entity.entity(
+                                    logInFailure
+                                    , MediaType.APPLICATION_JSON_TYPE
+                            )
+                    );
+            System.out.println(have.getStatus());
+            assertTrue(have.getStatus() == 404);
+        }
+    }
 
     private static String convertToJson(Object object) {
         ObjectMapper mapper = new ObjectMapper();
